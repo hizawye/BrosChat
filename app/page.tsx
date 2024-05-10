cd1017213957ef6b506fd75d5bae92082ce1df6d
Rone <safou348@gmail.com>
2024-05-11 00:41:57 +0100

    .

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,11 @@ import Image from "next/image";
 import { Chat } from "./Chat";
 
 export default function Home() {
-  return <Chat />;
+  return (
+    <div className="flex justify-center">
+      <div className="md:w-1/2">
+        <Chat />
+      </div>
+    </div>
+  );
 }
