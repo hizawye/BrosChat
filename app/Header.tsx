a9ff81fae6aaf40ce96c96ea3d9d8c09ffe98ba5
DoctorLimitless <safou348@gmail.com>
2024-05-14 17:36:58 +0100

    add header

--- app/Header.tsx
+++ app/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Header = () => {
   return (
-    <div className="fixed bg-white w-full text-center text-red-900 bg-opacity-90">
+    <div className="bg-white w-full text-center text-red-900 bg-opacity-90">
       <p>Bros-Chat</p>
     </div>
   );
