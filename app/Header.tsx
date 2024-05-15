8275643865f063fa296713a45246746ac2ac8554
DoctorLimitless <safou348@gmail.com>
2024-05-15 05:39:36 +0100

    adding title and desc

--- app/Header.tsx
+++ app/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Header = () => {
   return (
-    <div className="bg-white w-full text-center text-red-900 bg-opacity-90">
+    <div className="bg-white  text-center text-red-900 bg-opacity-90">
       <p>Bros-Chat</p>
     </div>
   );
